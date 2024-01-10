957db19b84cc7b060831dd129db380eb4ae9a061
sujithkp.ga <pendem.s@geekyants.com>
2024-01-10 13:12:02 +0530

    Update project with actual API

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 // script.js
-function changeTitle() {
-  document.getElementById("title").innerText = "Title Changed!";
+
+function getJoke() {
+  // Make a GET request to the joke API
+  fetch("https://v2.jokeapi.dev/joke/Programming?type=single")
+    .then((response) => response.json())
+    .then((data) => {
+      // Display the joke
+      console.log(data.joke);
+
+      // Add the joke to the title
+      document.getElementById("joke-line").innerText = data.joke;
+      document.getElementById("submit-button").innerText="Try another";
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
